cbf8183f1a520c51d591c03cc651f5005a52687b
Dzommer <philipp.sommer1@iu-study.org>
2024-09-13 15:22:47 +0200

    Update 6_price_calculation.R

--- scripts/6_price_calculation.R
+++ scripts/6_price_calculation.R
@@ -1,1 +1,15 @@
+# Berechnung des Preises basierend auf den Haushaltsausgaben
+base_price <- 12 # Basispreis in Deutschland
+spending_data_clean <- spending_data_clean %>%
+  mutate(price_in_country = base_price * (normalized_spending / spending_data_clean$normalized_spending[spending_data_clean$country == "Germany"]))
+
+# Plot der Preiskalkulation
+ggplot(spending_data_clean, aes(x=country, y=price_in_country)) +
+  geom_bar(stat="identity", fill="blue") +
+  coord_flip() +
+  ggtitle("Preiskalkulation der App in verschiedenen Ländern") +
+  xlab("Land") + ylab("Preis in €")
+
+# Speichern des Plots
+ggsave("output/price_comparison_plot.png")
 
