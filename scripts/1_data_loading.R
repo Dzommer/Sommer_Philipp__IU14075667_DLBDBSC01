9e6d85ac4801ced545911ce1a6a42624d09b067a
Dzommer <philipp.sommer1@iu-study.org>
2024-09-13 15:21:44 +0200

    Update 1_data_loading.R

--- scripts/1_data_loading.R
+++ scripts/1_data_loading.R
@@ -1,1 +1,8 @@
+# Einlesen der CSV-Daten
+broadband_data <- read.csv("data/breitbandzugang.csv")
+spending_data <- read.csv("data/haushaltsausgaben.csv")
+
+# Anzeigen der ersten Zeilen, um die Struktur zu überprüfen
+head(broadband_data)
+head(spending_data)
 
