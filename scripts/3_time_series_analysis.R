3c478a35b2eb81064ea3dd35aad8eb1e44e554ad
Dzommer <philipp.sommer1@iu-study.org>
2024-09-13 15:22:11 +0200

    Update 3_time_series_analysis.R

--- scripts/3_time_series_analysis.R
+++ scripts/3_time_series_analysis.R
@@ -1,1 +1,13 @@
+# Installiere erforderliche Pakete, falls notwendig
+if (!require(ggplot2)) install.packages("ggplot2")
+
+# Erstellen der Zeitreihe für die Breitbandabdeckung
+ggplot(broadband_data_clean, aes(x=year, y=broadband_access, group=country, color=country)) +
+  geom_line() +
+  ggtitle("Breitbandabdeckung in verschiedenen Ländern") +
+  xlab("Jahr") + ylab("Breitbandzugang (%)") +
+  theme_minimal()
+
+# Speichern des Plots
+ggsave("output/broadband_time_series_plot.png")
 
