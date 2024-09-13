d11eff87d0465dda6c59485e9c15933d9acf8687
Dzommer <philipp.sommer1@iu-study.org>
2024-09-13 15:22:20 +0200

    Update 4_correlation_analysis.R

--- scripts/4_correlation_analysis.R
+++ scripts/4_correlation_analysis.R
@@ -1,1 +1,9 @@
+# Berechnung der Pearson-Korrelation
+correlation <- cor(broadband_data_clean$broadband_access, spending_data_clean$normalized_spending)
+print(paste("Korrelationskoeffizient:", round(correlation, 2)))
+
+# Scatterplot erstellen
+plot(broadband_data_clean$broadband_access, spending_data_clean$normalized_spending,
+     xlab = "Breitbandzugang (%)", ylab = "Normierte Haushaltsausgaben",
+     main = paste("Korrelation zwischen Breitbandzugang und Haushaltsausgaben:", round(correlation, 2)))
 
